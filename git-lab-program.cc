42173ffd0ee7c307b059ae25098fdd48ae3a4dd7
zb273617 <59939233+zb273617@users.noreply.github.com>
2020-01-22 17:46:24 -0500

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zachary Bradshaw
+ *        Date: Jan 22 2020
+ * Description: printing out a Hello Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!\n";
+    return 0;
+}// main
